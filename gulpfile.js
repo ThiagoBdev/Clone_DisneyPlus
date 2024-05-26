fa39761b90980fafdaf4b5307582d2c51daf77ae
Thiago Batista <thiagobatista15390@gmail.com>
2024-05-26 15:56:31 -0300

    Criando o hero e importando as fontes Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const Gulp = require("gulp"); //guardando o gulp na variavel
 const Sass = require("gulp-sass")(require("sass"));  //guardando o sass na variavel
+const imagemin = require("gulp-imagemin");
+
 
 function styles() {
     return Gulp.src("./src/styles/*.scss")  //busca pelo arquivo 
@@ -7,9 +9,14 @@ function styles() {
     .pipe(Gulp.dest("./dist/css")); //envia o arquivo comprimido para a pasta de destino
 }
 
+function images () {
+    return Gulp.src("./src/images/**/*")  
+    .pipe(imagemin())
+    .pipe(Gulp.dest("./dist/images")); 
+}
 
+exports.default = Gulp.parallel(styles, images);
 
-exports.default = styles;
 exports.watch = function(){
     Gulp.watch("./src/styles/*.scss", Gulp.parallel(styles))
 }
