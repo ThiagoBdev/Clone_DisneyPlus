ae72e04a060beb488866ac8ab19447ffc58d5d92
Thiago Batista <thiagobatista15390@gmail.com>
2024-05-26 17:28:20 -0300

    criando a seção "shows"

---
+++ src/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const buttons = document.querySelectorAll("[data-tab-button]");
+    
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            EscondeAba();
+            aba.classList.add("shows__list--is--active");
+            removeBotaoativo();
+            botao.target.classList.add("shows__tabs__button--is--active");
+        })
+    }
+})
+
+function removeBotaoativo(){
+    const buttons = document.querySelectorAll("[data-tab-button]");
+
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove("shows__tabs__button--is--active");
+    }
+}
+
+function EscondeAba(){
+    const tabsContainer = document.querySelectorAll("[data-tab-id]");
+
+    for (let i = 0; i < tabsContainer.length; i++){
+        tabsContainer[i].classList.remove("shows__list--is--active");
+    }
+}
