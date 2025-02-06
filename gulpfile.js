d099473a11fd2687e0f899cc0c6b3780cab6fd7a
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-06 06:41:29 -0300

    criando a vercel.json

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,25 @@ const uglify = require("gulp-uglify");
 function scripts(){
     return Gulp.src("./src/scripts/*.js")
         .pipe(uglify())
-        .pipe(Gulp.dest("./dist/js"));
+        .pipe(Gulp.dest("./dist/public/js"));
 }
 
 
 function styles() {
     return Gulp.src("./src/styles/*.scss")  //busca pelo arquivo 
     .pipe(Sass({ outputStyle: "compressed" }))  //chama o sass e comprime o arquivo
-    .pipe(Gulp.dest("./dist/css")); //envia o arquivo comprimido para a pasta de destino
+    .pipe(Gulp.dest("./dist/public/css")); //envia o arquivo comprimido para a pasta de destino
 }
 
 function images () {
     return Gulp.src("./src/images/**/*")  
     .pipe(imagemin())
-    .pipe(Gulp.dest("./dist/images")); 
+    .pipe(Gulp.dest("./dist/public/images")); 
 }
 
 exports.default = Gulp.parallel(styles, images, scripts);
 
 exports.watch = function(){
     Gulp.watch("./src/styles/*.scss", Gulp.parallel(styles))
-    Gulp.watch("./src/scripts*.js", Gulp.parallel(scripts))
+    Gulp.watch("./src/scripts*/.js", Gulp.parallel(scripts))
 }
